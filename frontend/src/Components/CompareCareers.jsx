15be6d80a74a9c3982a11567ec16c4a3d8e6506d
JuheePatel <dollypatel26791@gmail.com>
2024-12-26 13:59:08 -0800

    Refactored code

---
+++ frontend/src/Components/CompareCareers.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import { postData } from '../api';
+import '../App.css';
+
+const CompareCareers = () => {
+    return (
+        <div className="container">
+            Compare Careers
+        </div>
+    );
+};
+
+export default CompareCareers;
---
+++ frontend/src/Components/SubmitSkill.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import { postData } from '../api';
+import '../App.css';
+
+const SubmitSkill = () => {
+    return (
+        <div className="container">
+            Submit Skill
+        </div>
+    );
+};
+
+export default SubmitSkill;
